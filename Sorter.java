003c2296a995b9f55b77c5329d6ff701d3e286ec
Caitlyn VGW <caitlyn.chan@vgw.co>
2019-08-12 17:45:41 +0800

    insertionSort now works

--- Sorter.java
+++ Sorter.java
@@ -6,7 +6,6 @@
  * Run program: java Sorter 
  */
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 
 public class Sorter { 
     /**
@@ -36,7 +35,7 @@ public class Sorter {
         long endTime = System.nanoTime(); 
         long time = (endTime - startTime); 
 
-        System.out.println("Execution time was: " + time + " nanoseconds");
+        System.out.println("Execution time was: " + time + " nanoseconds for insertionSort" );
 
     }
     /**
@@ -47,76 +46,80 @@ public class Sorter {
      * @param end ending index
      * references: CITS2200 Sorter lab by yours truly ~ Caitlyn 
      */
-    public void quickSort(int [] arr, int start, int end){
+    // public void quickSort(int [] arr, int start, int end){
 
-        if (start < end){ 
+    //     if (start < end){ 
 
-            int splitIndex = partition(arr, start, end); //splits original unsorted array 
-            quickSort(arr, start, splitIndex - 1); //sorts first half of array (+/- 1 due to indexing)
-            quickSort(arr, splitIndex + 1 , end); //sorts back half of array 
-        }
-    }
-    public int partition(int[]arr, int start, int end){
+    //         int splitIndex = partition(arr, start, end); //splits original unsorted array 
+    //         quickSort(arr, start, splitIndex - 1); //sorts first half of array (+/- 1 due to indexing)
+    //         quickSort(arr, splitIndex + 1 , end); //sorts back half of array 
+    //     }
+    // }
+    // public int partition(int[]arr, int start, int end){
         
-        int sortIndex = arr[end];  //using last element of the array to be used as the pivot 
-        int startingIndex  = start - 1; // gets starting index 
+    //     int sortIndex = arr[end];  //using last element of the array to be used as the pivot 
+    //     int startingIndex  = start - 1; // gets starting index 
 
-        for (int i = start; i <= (end-1); i++){ 
+    //     for (int i = start; i <= (end-1); i++){ 
 
-            if(arr[startingIndex] <= sortIndex) { //if starting index element is less than ending index element
-                i++; // move along cotton eye joe 
-                int temp = arr[startingIndex]; //temp element to assign it to the current smaller element     
-                arr[startingIndex] = arr[i];
-                arr[i] = temp; 
-            }
-        }
-        int temp = arr[startingIndex + 1]; // 
-        arr[startingIndex + 1] = arr[end]; //swap and make changes to the actual array 
-        arr[end] = temp;
+    //         if(arr[startingIndex] <= sortIndex) { //if starting index element is less than ending index element
+    //             i++; // move along cotton eye joe 
+    //             int temp = arr[startingIndex]; //temp element to assign it to the current smaller element     
+    //             arr[startingIndex] = arr[i];
+    //             arr[i] = temp; 
+    //         }
+    //     }
+    //     int temp = arr[startingIndex + 1]; // 
+    //     arr[startingIndex + 1] = arr[end]; //swap and make changes to the actual array 
+    //     arr[end] = temp;
 
-        return (startingIndex +1); 
-    } 
+    //     return (startingIndex +1); 
+    // } 
 
-    /**
-     * Private method to merge the elements in the array between start and the end. 
-     * the array arr , between the indices start and end inclusive 
-     * Given the array is sorted between 
-     */
-    private void merge(int[] arr, int start, int middle, int end) {
+    // /**
+    //  * Private method to merge the elements in the array between start and the end. 
+    //  * the array arr , between the indices start and end inclusive 
+    //  * Given the array is sorted between 
+    //  */
+    // private void merge(int[] arr, int start, int middle, int end) {
 
-        //gGetting size of temp left and right arrays 
-        int leftSize = (middle - start) + 1 ; 
-        int rightSize = end - middle; 
+    //     //gGetting size of temp left and right arrays 
+    //     int leftSize = (middle - start) + 1 ; 
+    //     int rightSize = end - middle; 
 
-        //Creating temp left and right arrays 
-        int leftArray[] = new arr[leftSize];
-        int rightArray[] = new arr[rightSize];
+    //     //Creating temp left and right arrays 
+    //     int leftArray[] = new arr[leftSize];
+    //     int rightArray[] = new arr[rightSize];
 
-        //Copying data to temp left and right arrays 
-        for(int i = 0; i < left; i++){
-            leftArray[i] = arr[start + i];
-        }
-        for(int j = 0; j < right; j++){
-            rightArray[j] = arr[middle + j + 1];
-        }
-    }
-    /**
-     * Divide and conquer algorithm -> division into sub problems 
-     */
-    public void mergeSort(int [] arr, int start , int end) {
+    //     //Copying data to temp left and right arrays 
+    //     for(int i = 0; i < leftSize; i++){
+    //         leftArray[i] = arr[start + i];
+    //     }
+    //     for(int j = 0; j < rightSize; j++){
+    //         rightArray[j] = arr[middle + j + 1];
+    //     }
+    // }
+    // /**
+    //  * Divide and conquer algorithm -> division into sub problems 
+    //  */
+    // public void mergeSort(int [] arr, int start , int end) {
 
-        long startTime = System.nanoTime();
+    //     long startTime = System.nanoTime();
 
-        if( start < end ) {
-            int middle = (start + end)/2;  
-            mergeSort(arr, start, middle);
-            mergeSort(arr, middle + 1, end);
-            merge(arr, start, middle, end);
-        }
+    //     if( start < end ) {
+    //         int middle = (start + end)/2;  
+    //         mergeSort(arr, start, middle);
+    //         mergeSort(arr, middle + 1, end);
+    //         merge(arr, start, middle, end);
+    //     }
 
-        long endTime = System.nanoTime(); 
+    //     long endTime = System.nanoTime(); 
 
-    }
+    //     long time = (endTime - startTime); 
+
+    //     System.out.println("Execution time was: " + time + " nanoseconds for Merge Sort\n");
+
+    // }
 
     // public void heapSort(){
 
